eef60e2696c2752c91622bb8b78c83fd37736cbb
somename <someemail>
2015-10-15 11:43:25 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,12 +3,20 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.Frame;
 
 public class TestBowlingScoreCalculator {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void frame_should_have_two_throws() {
+		int first = 1;
+		int second = 2;
+		Frame frame = new Frame(first, second);
+		
+		assertEquals(first, frame.getFirstThrow());
+		assertEquals(second, frame.getSecondThrow());
+		
+	
 	}
 
 }
