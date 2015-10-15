c0366cd355e68169a39dbb771d69b8ac6e7bfb97
somename <someemail>
2015-10-15 11:50:57 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -4,6 +4,10 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	
+	public Frame() {
+		
+	}
+	
 	public Frame(int firstThrow, int secondThrow){
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
@@ -36,6 +40,14 @@ public class Frame {
 	public int bonus(){
 		//to be implemented
 		return 0;
+	}
+	
+	public void setFirstThrow(int score) {
+		this.firstThrow = score;
+	}
+	
+	public void setSecondThrow(int score) {
+		this.secondThrow = score;
 	}
 	
 	public int getFirstThrow() {
